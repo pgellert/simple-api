3eea730d98155d997adc061c9e4c11fa713db2ea
Gellért Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2019-02-06 19:07:51 +0000

    Bug fixes

--- app/app/views.py
+++ app/app/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse
 import urllib
-import markdown
-import os
+import markdown2
+
 
 def index(request):
     with urllib.request.urlopen('https://raw.githubusercontent.com/pgellert/simple-api/master/README.md') as response:
-        content = response.read()
-        return HttpResponse(markdown.markdown(content))
+        content = response.read().decode('utf-8')
+        return HttpResponse(markdown2.markdown(content))
+
 
 def grocery_list(request):
     return HttpResponse("Hello! This is the URI for the grocery list")
+
 
 def grocery_by_name(request, name):
     return HttpResponse("Hello! This is the URI for the " + name)
